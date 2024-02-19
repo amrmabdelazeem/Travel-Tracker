fbda1bedff40f75d99041f7a1ba852a01e01bc5b
amrmabdelazeem <amr.abdelazeem22@gmail.com>
2024-02-19 16:29:44 +0300

    Add SQL queries

--- index.js
+++ index.js
@@ -7,55 +7,51 @@ const port = 3000;
 
 const db = new pg.Client({
   user: "postgres",
+  host: "localhost",
   port: 5432,
   database: "world",
   password: "789121",
-  host: "localhost",
 });
 
 db.connect();
 
-let totalCount = 0;
-
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-async function selectVisited(){
-  let countries = [];
-  const result = await db.query("SELECT * FROM visited_countries");
 
-  result.rows.forEach((country) => {
+async function checkVisited(){
+
+  const result = await db.query("SELECT country_code FROM visited_countries");
+  let countries = [];
+  result.rows.forEach((country)=>{
     countries.push(country.country_code);
+    
   });
   return countries;
 }
 
 app.get("/", async (req, res) => {
-  //Write your code here.
-  const countries = await selectVisited();
+  const countries = await checkVisited();
+  console.log(countries.length);
   res.render("index.ejs", { countries, total: countries.length });
+  // db.end();
 });
 
-app.post("/add", async (req, res) => {
-  const newCountry = req.body["country"];
-  console.log(newCountry);
-  let result = await db.query("SELECT country_code FROM countries WHERE country_name=$1", [newCountry]);
-  if(result.rows.length !== 0){
+app.post("/add", async (req, res)=>{
+  const input = req.body["country"];
+  const result = await db.query("SELECT country_code From countries WHERE country_name=$1",[input]);
+  if(result.rows.length !==0){
     const data = result.rows[0];
     const countryCode = data.country_code;
-    console.log(result.rows[0].country_code);
 
-  await db.query("INSERT INTO visited_countries (country_code) Values($1)",[countryCode]);
-  res.redirect("/");
+    db.query("INSERT INTO visited_countries (country_code) VALUES($1)",[countryCode]);
+    res.redirect("/");
   }
-  
 });
 
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
 });
-
-
 
 /*
 1- Assign a route "/add"
