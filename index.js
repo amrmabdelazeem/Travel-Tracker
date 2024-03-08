1fc07302712bcf907e475a65e6a7c903c8f7bae7
amrmabdelazeem <amr.abdelazeem22@gmail.com>
2024-03-08 15:45:54 +0300

    minor changes

--- index.js
+++ index.js
@@ -10,9 +10,9 @@ const port = 3000;
 const db = new pg.Client({
   user: "postgres",
   host: "localhost",
-  port: 5432,
-  database: "world",
-  password: "789121",
+  port: 'add_port',
+  database: 'database_name', //Change this!
+  password: 'password', //Change this!
 });
 
 db.connect();
